d65c82d0ee2d3b3451ee7e0588d4c630c145f08f
Clément Laroumagne <clement.laroumagne1@etu.unilim.fr>
2025-11-07 10:58:56 +0100

    fix nextconfig

--- clementine2/next.config.ts
+++ clementine2/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/clementine',
-  assetPrefix: '/clementine/',
   images: {
     unoptimized: true,
   },
